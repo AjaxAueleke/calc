5105930c122a6c37d5b357d5b6bfa913ab594300
ahmed.jamil7410@gmail.com <ahmed.jamil7410@gmail.com>
2021-05-04 17:17:12 +0500

    Calculator

---
+++ app.js
@@ -0,0 +1,12 @@
+function num(n)
+{
+    document.getElementById('calc').value += n;
+};
+function Eval()
+{
+    document.getElementById('calc').value = eval(document.getElementById('calc').value);
+}
+function del()
+{
+    document.getElementById('calc').value = document.getElementById('calc').value.slice(0, document.getElementById('calc').value.length -1);
+}
